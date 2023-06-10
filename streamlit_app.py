54cc283a3b202ab180982d808378dd831f072627
holiday-tun <133661315+holiday-tun@users.noreply.github.com>
2023-06-10 20:00:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,59 @@ import math
 import pandas as pd
 import streamlit as st
 
+import openai
+
+import json
+
+import requests
+
+import streamlit as st
+
+openai.api_key="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"
+
+def generate_headline(input_text):
+
+    completion = openai.Completion.create(
+
+        model="text-davinci-002",
+
+        prompt="""given a topic provided by the user, generate at least five attention grabbing titles and headlines on different perspectives that follow specific formats,
+
+               one title should use specific numbers and data in the headline. It should utilize unique rationales like ways, tips, tricks, ideas, facts, and similar words.Another title should use emotional words and provide a sense of urgency.The next title should try to use all the headline formulas.Two additional titles should follow any other format of your choice.
+
+                titles and headlines should be short, clear, and direct, as well as unexpected and surprising. They should aim to catch the reader's attention and be highly shareable. The user can then select which title suits their needs the best:""",
+
+        temperature=0.7,
+
+        max_tokens=60,
+
+        top_p=1,
+
+        frequency_penalty=0,
+
+        presence_penalty=0
+
+    )
+
+    headline = completion.choices[0].text
+
+    return headline
+
+if st.button("Generate Headline"):
+
+    input_text = st.text_input("Enter a topic:")
+
+    if input_text:
+
+        with st.spinner('Generating your headline....'):
+
+            result = generate_headline(input_text)
+
+            st.text(result)
+
+            st.success('done')
+
+
 """
 # Welcome to Streamlit!
 
